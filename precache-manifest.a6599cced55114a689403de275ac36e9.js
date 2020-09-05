49353064012f9586ea53b5ec652226e1a992d7fd
Prafull <prafullkumar41@gmail.com>
2020-09-05 16:12:47 +0530

    Updates

---
+++ precache-manifest.a6599cced55114a689403de275ac36e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a8c243cd105ecc201bdeac5e6f07ee",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "6f913b1299ec93dd7045",
+    "url": "/currency-converter/static/css/main.23d0b274.chunk.css"
+  },
+  {
+    "revision": "dd42657d8707c02daf65",
+    "url": "/currency-converter/static/js/2.b94c56cd.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/currency-converter/static/js/2.b94c56cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f913b1299ec93dd7045",
+    "url": "/currency-converter/static/js/main.abb1935c.chunk.js"
+  },
+  {
+    "revision": "d948aae945650cc39730",
+    "url": "/currency-converter/static/js/runtime-main.ce71e974.js"
+  }
+]);
